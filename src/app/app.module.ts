e3df5c679772fa0ac73593c4f17a765a6943db29
Ljiljana <ljiljana.skavic@hotmail.com>
2023-05-19 10:15:10 +0200

    Add page not found component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { MatButtonModule } from "@angular/material/button";
 import { MatInputModule } from "@angular/material/input";
 import { MatIconModule } from "@angular/material/icon";
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 const MATERIAL_MODULES = [
   MatCardModule,
@@ -31,6 +32,7 @@ const MATERIAL_MODULES = [
     ProductCardComponent,
     LoginCardComponent,
     SignUpCardComponent,
+    PageNotFoundComponent,
   ],
   imports: [
     BrowserModule,
