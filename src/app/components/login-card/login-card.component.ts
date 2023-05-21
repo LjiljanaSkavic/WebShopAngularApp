254b9e97a128e03419886fb7670a60758134474b
Ljiljana <ljiljana.skavic@hotmail.com>
2023-05-21 18:37:38 +0200

    Add shared service

--- src/app/components/login-card/login-card.component.ts
+++ src/app/components/login-card/login-card.component.ts
@@ -7,6 +7,7 @@ import {
   UntypedFormGroup,
   Validators
 } from "@angular/forms";
+import { SharedService } from "../../services/shared.service";
 
 @Component({
   selector: 'app-login-card',
@@ -15,9 +16,9 @@ import {
 })
 export class LoginCardComponent implements OnInit {
   hidePassword = true;
-  loginForm: UntypedFormGroup;;
+  loginForm: UntypedFormGroup;
 
-  constructor(private readonly _formBuilder: UntypedFormBuilder,) {
+  constructor(private readonly _formBuilder: UntypedFormBuilder, private sharedService: SharedService) {
   }
 
   ngOnInit(): void {
@@ -34,15 +35,17 @@ export class LoginCardComponent implements OnInit {
   }
 
   onLoginClick($event: MouseEvent) {
-
+    //TODO: post sa username and password
   }
 
-   buildForm() {
+  buildForm() {
     this.loginForm = this._formBuilder.group({
       username: new UntypedFormControl('', [Validators.required]),
       password: new UntypedFormControl('', [Validators.required])
-      }
-    )
+    });
+  }
 
+  getPasswordHash(): string {
+    return this.sharedService.getSha512Hash(this.loginForm.get('password')?.value);
   }
 }
