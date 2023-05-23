5c2e22335f9e86860fb9cdfaf4388df10392821e
Ljiljana <ljiljana.skavic@hotmail.com>
2023-05-23 11:53:47 +0200

    Add comment field for products with animations

--- src/app/components/product-card/product-card.component.ts
+++ src/app/components/product-card/product-card.component.ts
@@ -1,18 +1,37 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Product } from "../../models/Product";
+import { UserService } from "../../services/user.service";
+import { animate, AUTO_STYLE, state, style, transition, trigger } from "@angular/animations";
+
+let DEFAULT_DURATION = 300;
 
 @Component({
   selector: 'app-product-card',
   templateUrl: './product-card.component.html',
-  styleUrls: ['./product-card.component.scss']
+  styleUrls: ['./product-card.component.scss'],
+  animations: [
+    trigger('collapse', [
+      state('false', style({height: AUTO_STYLE, visibility: AUTO_STYLE})),
+      state('true', style({height: '0', visibility: 'hidden'})),
+      transition('false => true', animate(DEFAULT_DURATION + 'ms ease-in')),
+      transition('true => false', animate(DEFAULT_DURATION + 'ms ease-out'))
+    ])
+  ]
 })
 export class ProductCardComponent implements OnInit {
   @Input() product: Product;
+  isLoggedIn: boolean;
+  collapsed = true;
 
-  constructor() {
+  constructor(private userService: UserService) {
   }
 
   ngOnInit(): void {
+    this.isLoggedIn = this.userService.isLoggedIn;
+  }
+
+  toggle() {
+    this.collapsed = !this.collapsed;
   }
 
 }
