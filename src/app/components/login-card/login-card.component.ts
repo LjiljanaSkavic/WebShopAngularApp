e45ce4a0f518e15f401001d283dc8c60db2ac212
Ljiljana <ljiljana.skavic@hotmail.com>
2023-05-24 11:30:07 +0200

    Add routings for sign up and login

--- src/app/components/login-card/login-card.component.ts
+++ src/app/components/login-card/login-card.component.ts
@@ -8,6 +8,7 @@ import {
   Validators
 } from "@angular/forms";
 import { SharedService } from "../../services/shared.service";
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-login-card',
@@ -18,30 +19,32 @@ export class LoginCardComponent implements OnInit {
   hidePassword = true;
   loginForm: UntypedFormGroup;
 
-  constructor(private readonly _formBuilder: UntypedFormBuilder, private sharedService: SharedService) {
+  constructor(private readonly _formBuilder: UntypedFormBuilder, private sharedService: SharedService, private router: Router) {
   }
 
   ngOnInit(): void {
     this.buildForm();
     this.loginForm = new FormGroup({
-      username: new FormControl(''),
-      password: new FormControl('')
+      username: new FormControl(null),
+      password: new FormControl(null)
     })
   }
 
 
   onSignUpClick($event: MouseEvent) {
-
+    this.router.navigateByUrl('sign-up').then(r => console.log('sign-up'));
   }
 
   onLoginClick($event: MouseEvent) {
-    //TODO: post sa username and password
+    const username = this.loginForm.get('username')?.value;
+    const password = this.getPasswordHash();
+    console.log(username, password);
   }
 
   buildForm() {
     this.loginForm = this._formBuilder.group({
-      username: new UntypedFormControl('', [Validators.required]),
-      password: new UntypedFormControl('', [Validators.required])
+      username: new UntypedFormControl(null, [Validators.required]),
+      password: new UntypedFormControl(null, [Validators.required])
     });
   }
 
--- src/app/components/sign-up-card/sign-up-card.component.ts
+++ src/app/components/sign-up-card/sign-up-card.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-sign-up-card',
@@ -7,6 +8,13 @@ import { Component } from '@angular/core';
 })
 export class SignUpCardComponent {
 
+  constructor(private router: Router) {
+  }
+
   onLogIn($event: MouseEvent) {
+    this.router.navigateByUrl('login').then(r => console.log('login'));
+  }
+
+  onSignUp($event: MouseEvent) {
   }
 }
