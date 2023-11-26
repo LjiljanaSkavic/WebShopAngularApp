5ad5b26a708726d9318b0f4b7e42cfb752e0681e
Ljiljana Skavic <ljiljana.skavic@hotmail.com>
2023-11-26 22:54:54 +0100

    Add shared classes and colors to every component

--- src/app/components/contact-support-modal/contact-support-modal.component.ts
+++ src/app/components/contact-support-modal/contact-support-modal.component.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core';
-import { FormControl, FormGroup } from "@angular/forms";
-import { Subscription } from "rxjs";
-import { DIALOG_RESPONSE } from "../confirmation-modal/confirmation-modal.component";
+import {Component} from '@angular/core';
+import {FormControl, FormGroup} from "@angular/forms";
+import {Subscription} from "rxjs";
+import {DIALOG_RESPONSE} from "../confirmation-modal/confirmation-modal.component";
 
 @Component({
   selector: 'app-contact-support-modal',
